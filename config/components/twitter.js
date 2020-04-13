e5522daadc12390be0f9e4cbb8c3f8d2baeb3bac
Alexis Araujo <ala1289@gmail.com>
2020-04-12 20:16:57 -0500

    Add compose services api and load data and redis

--- config/components/twitter.js
+++ config/components/twitter.js
@@ -7,7 +7,7 @@ const config = {
     consumerSecret: 'LBgKjFFu4WCyx35Kfm0yfvcmPPUay2ogZE0S7rQGhGVWCBpXap',
     accessTokenKey: '51149651-UBBNhI080KD3nuKw6d2jjN8oy3FIqFqLxW8CvKCk8',
     accessTokenSecret: 'ieTmF1WcKqYAXaQm4YShs15vEPeYA0qXo0IIVS8wLIadO',
-    track: 'Chumel',
+    track: process.env.TWITTER_TRACK,
   },
 };
 
