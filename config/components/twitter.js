9373aeb7aee72c022d603ad806aff771d57286a8
Alexis Araujo <ala1289@gmail.com>
2020-04-12 20:41:38 -0500

    Fix variables config

--- config/components/twitter.js
+++ config/components/twitter.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const config = {
   twitter: {
-    consumerKey: 'g8OHgY6NR8H7qaIOpOq0phRjT',
-    consumerSecret: 'LBgKjFFu4WCyx35Kfm0yfvcmPPUay2ogZE0S7rQGhGVWCBpXap',
-    accessTokenKey: '51149651-UBBNhI080KD3nuKw6d2jjN8oy3FIqFqLxW8CvKCk8',
-    accessTokenSecret: 'ieTmF1WcKqYAXaQm4YShs15vEPeYA0qXo0IIVS8wLIadO',
+    consumerKey: process.env.TWITTER_CONSUMER_KEY,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+    accessTokenKey: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
     track: process.env.TWITTER_TRACK,
   },
 };
