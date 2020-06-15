99bf294a8d92e50834b3ec322cfdfa171d7efc1a
Alexis Araujo <ala1289@gmail.com>
2020-06-15 16:53:12 -0500

    Create connection redis

--- index.js
+++ index.js
@@ -5,19 +5,47 @@ const app = express();
 const port = 3000;
 
 const axios = require('axios');
+const redis = require('redis');
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/recipe/:fooditem', async (req, res) => {
+// make a connection to the local instance of redis
+const client = redis.createClient(6379, 'redis');
+
+client.on('error', (error) => {
+  console.error(error);
+});
+
+app.get('/recipe/:fooditem', (req, res) => {
   try {
-    const fooditem = req.params.fooditem;
-    const recipe = await axios.get(
-      `http://www.recipepuppy.com/api/?q=${fooditem}`,
-    );
-    return res.status(200).send({
-      error: false,
-      data: recipe.data.results,
+    const foodItem = req.params.fooditem;
+
+    // Check the redis store for the data first
+    client.get(foodItem, async (err, recipe) => {
+      if (recipe) {
+        return res.status(200).send({
+          error: false,
+          message: `Recipe for ${foodItem} from the cache`,
+          data: JSON.parse(recipe),
+        });
+      } else {
+        // When the data is not found in the cache then we can make request to the server
+
+        const recipe = await axios.get(
+          `http://www.recipepuppy.com/api/?q=${foodItem}`,
+        );
+
+        // save the record in the cache for subsequent request
+        client.setex(foodItem, 1440, JSON.stringify(recipe.data.results));
+
+        // return the result to the client
+        return res.status(200).send({
+          error: false,
+          message: `Recipe for ${foodItem} from the server`,
+          data: recipe.data.results,
+        });
+      }
     });
   } catch (error) {
     console.log(error);
